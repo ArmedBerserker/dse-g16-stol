269f128bd87e04abf80769cf2db12728eddd6e82
ArmedBerserker <janviernele@gmail.com>
2026-05-04 15:07:53 +0200

    added intial fuel fraction calculations

--- c1_m.py
+++ c1_m.py
@@ -1,22 +1,79 @@
 import class1.prelim_drag as drag1
-from classes.aircraft_2 import Aircraft
+from classes.aircraft_2 import loader, Aircraft
 from lookups.consts import *
+import pandas as pd
+import numpy as np
 
-def taxi_frac(ac : Aircraft) -> float:
+class Class1:
+    def __init__(self, ac : Aircraft, frac_source : str = 'lookups/fuel_fracs1.csv'):
+        self.ac = ac
+        if ac.requirements.general['standard_type'] == None:
+            print('No standard aircraft type found. Using Homebuilt.')
+            self.ac_type = 'Homebuilt'
+        else:
+            self.ac_type = ac.general.requirements['standard_type']
+
+        self.data = pd.read_csv(frac_source)
+        self.rel_data = self.data[self.data['Airplane Type'] == self.ac_type].iloc[0]
     
-def takeoff_frac(ac : Aircraft) -> float:
-    ...
-def climb_frac(ac : Aircraft) -> float:
-    ...
-def cruise_frac(ac : Aircraft) -> float:
-    ...
-def descent_frac(ac : Aircraft) -> float:
-    ...
-def loiter_frac(ac : Aircraft) -> float:
-    ...
-def landing_frac(ac : Aircraft) -> float:
-    ...
+    @property
+    def _warm_up_frac(self) -> float:
+        return self.rel_data['Warm-up']
+    
+    @property
+    def _taxi_frac(self) -> float:
+        return self.rel_data['Taxi']
+    
+    @property
+    def _take_off_frac(self) -> float:
+        return self.rel_data['Take-off']
+    
+    @property
+    def _climb_frac(self) -> float:
+        return self.rel_data['Climb']
+    
+    @property
+    def _descent_frac(self) -> float:
+        return self.rel_data['Descent']
+    
+    @property
+    def _landing_frac(self) -> float:
+        return self.rel_data['Landing Taxi']
+    
+    @property
+    def _fuel_frac(self) -> float:
+        engine_type = self.ac.engine.engine_type
+        if engine_type == 'prop':
+            return self.breguet_prop(self)
+        else:
+            return self.breguet_jet(self)
+        
+    def breguet_prop(self):
+        R = self.ac.mission.range / MIL_TO_KM
+        eta_prop = self.ac.engine.eta_prop
+        c_p = self.ac.engine.c_p # assumed in lbs/hr/hp
+        lnfrac = R / 375 * c_p / eta_prop / self.ac.wing.ld
+        fracinv = np.exp(lnfrac)
+        return 1 / fracinv
+    
+    def breguet_jet(self):
+        R = self.ac.mission.range / NMIL_TO_KM
+        V = self.ac.mission.cruise_speed  # as long as v_cr in kts
+        c_j = self.ac.engine.c_j
+        lnfrac = R * c_j / V / self.ac.wing.ld
+        fracinv = np.exp(lnfrac)
+        return 1/fracinv
+    
+    @property
+    def fuel_frac_used(self):
+        mulitple = self._warm_up_frac * self._taxi_frac * self._take_off_frac * self._climb_frac * self._fuel_frac * self._descent_frac * self._landing_frac
+        return (1 - mulitple) * self.ac.weights.m_takeoff + self.ac.requirements.general['reserve']
+    
 
 
-def class1_mass(ac : Aircraft) -> float:
-    ...
+    
+
+    
+
+        
+        
--- lookups/consts.py
+++ lookups/consts.py
@@ -3,3 +3,5 @@ Constants used for unit conversions in aircraft sizing calculations.
 """
 LBS_TO_KG = 0.453592
 M2_TO_F2 = 10.764
+MIL_TO_KM = 1.60934
+NMIL_TO_KM = 1.852
