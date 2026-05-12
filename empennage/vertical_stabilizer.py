03b6c3f9bdedb985292479b585d3d36f9da38d80
tvanwoensel <t.p.z.vanwoensel@student.tudelft.nl>
2026-05-12 16:47:14 +0200

    aldi

--- empennage/vertical_stabilizer.py
+++ empennage/vertical_stabilizer.py
@@ -1,20 +1,33 @@
-# TODO: link these to yaml files
-AR = 11
-MAC = 1.5
-S = 23
-l_f = 8.9
+import math
 
-X_aft_cg = 1 # TBD
+# --- Constants / Design Choices (TODO: link these to yaml files) ---
+    X_v_factor = 0.9
+    V_v = 0.06275      # Roskam volume coefficient
+    AR_v = 1.5         # ADSEE average aspect ratio
+    Sweep_v = 25       # degrees
+    lambda_v = 0.4     # ADSEE taper ratio
+    T_over_C_v = 0.15  # Roskam thickness-to-chord
 
-X_v = 0.9*l_f
-V_v = 0.06275 # from roskam
-AR_v = 1.5 # avg from adsee
-Sweep_v = 25 # degrees
-lambda_v = 0.4 # from adsee
+def calculate_vertical_stabilizer_geometry(l_f, MAC, S, X_aft_cg=1.0):
+   
+    # --- Calculations ---
+    X_v = X_v_factor * l_f
+    
+    # Vertical tail area based on volume coefficient formula
+    # S_v = (V_v * MAC * S) / (X_v - X_aft_cg)
+    S_v = (0.36 * MAC) / (X_v - X_aft_cg) * S 
+    
+    b_v = (AR_v * S_v)**0.5
+    Cr_v = (2 * S_v) / (b_v * (1 + lambda_v))
+    Ct_v = lambda_v * Cr_v
+    
+    return {
+        "S_v": S_v,
+        "b_v": b_v,
+        "Cr_v": Cr_v,
+        "Ct_v": Ct_v,
+        "X_v": X_v,
+        "AR_v": AR_v,
+        "Sweep_v": Sweep_v
+    }
 
-T_over_C_v = 0.15 # from roskam
-
-S_v = (0.36*MAC)/(X_v-1)*S 
-b_v = (AR_v*S_v)**0.5
-Cr_v = (2*S_v)/(b_v*(1+lambda_v))
-Ct_v = lambda_v*Cr_v
